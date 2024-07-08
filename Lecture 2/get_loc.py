b6c4391f80488d22484cd5d85edb10910377aa57
Mohamed Osama <mohammad.osamaa25@gmail.com>
2024-07-08 17:05:21 +0300

    [python-tasks]

--- Lecture 2/get_loc.py
+++ Lecture 2/get_loc.py
@@ -3,5 +3,5 @@ from pprint import pprint
 response=requests.get("https://api.ipify.org/?format=json")
 ip = response.json()["ip"]
 response=requests.get(f"https://ipinfo.io/{ip}/geo")
-loc = response.json()
-pprint(loc)
+location = response.json()
+pprint(location)
